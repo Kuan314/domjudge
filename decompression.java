463e8691bf37667d8d5e2dbb60d856b58f158ce2
Kuan314 <limzhengkuan@hotmail.com>
2021-01-04 02:23:58 +0800

    Update decompression.java

--- decompression.java
+++ decompression.java
@@ -4,48 +4,35 @@ public class decompression {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-      char[] word = sc.nextLine().toCharArray();
-      String output = "";
-      if(word.length <= 1024){
-          for(int i = 0; i < word.length; i++){
-              boolean check = false;
-              int tem = 0;
-              if(word[i] == '0')
-                  break;
+        String word = sc.next();
+        String output = "";
+        int i = 0;
 
-              int checkNum = 0;
-              while(i < word.length && word[i] >= '0' && word[i] <= '9'){
-                  tem += word[i] - '0';
-                  if(check){
-                      tem *= 10;
-                  }
-                  i++;
-                  checkNum++;
-                  check = true;
-              }
-              i--;
-              String s = "";
+        while(i < word.length()){
+            if(Character.isDigit(word.charAt(i))){
+                int start = i;
+                while(Character.isDigit(word.charAt(i))){
+                    i++;
+                }
+                int num = Integer.parseInt(word.substring(start, i));
 
-              if(word.length > checkNum){
-                  if(tem >= 1 && tem <= 1000 ){
-                      do{
-                          i++;
-                          checkNum++;
-                          s += word[i];
-                          System.out.println(checkNum);
-                      }while(i + 1 < word.length && word[i+1] >= 'a' && word[i+1] <= 'z' || i + 1 < word.length && word[i+1] >= 'A' && word[i+1] <= 'Z');
-                  }
-                  else{
-                      break;
-                  }
-
-                  while(tem != 0){
-                      output += s;
-                      tem--;
-                  }
-              }
-          }
-        System.out.println(output);
+                String temp = "";
+                while(Character.isLetter(word.charAt(i))){
+                    temp += word.charAt(i);
+                    i++;
+                    if(i == word.length()){
+                        break;
+                    }
+                }
+                for(int j = 0; j < num; j++){
+                    output += temp;
+                }
+            }
+            else{
+                i++;
+            }
         }
+        System.out.println(output);
+
     }
 }
